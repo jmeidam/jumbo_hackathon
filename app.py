df2be07d935038e09f8dff8c9d16594fb110cbb7
Jeroen Meidam <jeroen.meidam@qnh.nl>
2021-09-10 14:41:08 +0200

    adds the app

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+
+class Product:
+    def __init__(self, parameter1, parameter2, package):
+        self.parameter1 = parameter1
+        self.parameter2 = parameter2
+        self.package = package
+
+    def get_green_score(self):
+        return self.parameter1 + self.parameter2
+
+
+class Receipt:
+    def __init__(self, products):
+        self.products = products
+
+    def add_products(self, products):
+        self.products.append(products)
+
+    def calculate_green_score(self):
+        score = 0
+        for product in self.products:
+            score += product.get_green_score()
+        return score
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def whatever():
+    value = 5
+    return f"<p>Test = {5}</p>"
